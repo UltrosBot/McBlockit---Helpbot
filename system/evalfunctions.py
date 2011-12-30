5dff6796fc70c4b8076420fdc3fe2674591dfd4c
gdude2002 <colesgareth2@hotmail.com>
2011-12-30 23:23:44 +0000

    HTTPMessage instance has no attribute 'split'

--- system/evalfunctions.py
+++ system/evalfunctions.py
@@ -38,8 +38,8 @@ class evalFunctions(object):
         obj = urllib.urlopen(url)
         if obj.geturl().startswith("file://"):
             return "Local file access is not allowed."
-        elif obj.info().split("Content-Type: ")[1].lower() != "text/html":
-            return "Content-Type " + obj.info().split("Content-Type: ")[1] + "not allowed."
+        elif str(obj.info()).split("Content-Type: ")[1].lower() != "text/html":
+            return "Content-Type " + str(obj.info()).split("Content-Type: ")[1] + "not allowed."
         else:
             return obj.read()
 
