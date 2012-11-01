6bc2206bc918a7c35c064fd253ea84a9ebaa87b8
Gareth Coles <gdude2002@pageserved.com>
2012-11-01 01:39:26 +0000

    Improved and renamed .fetchtitle
    * Renamed to .title
    * Bot now stores last parsed URL, .title on its own will reparse it
    * Channels config for urlparser has changed; but there is no need to edit
      it. This time. (see #13)

--- plugins/urltitlefetch.py
+++ plugins/urltitlefetch.py
@@ -9,7 +9,7 @@ class plugin(object):
 
     commands = {
         "urltitle": "url_options",
-        "fetchtitle": "fetch_title",
+        "title": "fetch_title",
     }
 
     hooks = {
@@ -22,6 +22,8 @@ class plugin(object):
     notParse = ["png", "jpg", "jpeg", "tiff", "bmp", "ico", "gif", "iso", "bin", "pub", "ppk", "doc", "docx", "xls",
                 "xlsx", "ppt", "pptx", "svg"]
 
+    channels = {}
+
     def __init__(self, irc):
         self.irc = irc
         self.settings = yaml_loader(True, "urltitlefetcher")
@@ -32,16 +34,18 @@ class plugin(object):
             "urltitle": "Toggle url title fetching for the current channel.\n" +
                         ("Usage: %surltitle [all|on|off]\n" % self.irc.control_char) +
                         "all = all users, on = voiced and up, off = no-one",
-            "fetchtitle": "Fetch the title of a given URL.\n" +
-                     ("Usage: %sfetchtitle <url>\n" % self.irc.control_char) +
+            "title": "Fetch the title of a given URL.\n" +
+                     ("Usage: %stitle <url>\n" % self.irc.control_char) +
                      "NOTE: If title fetching is off in the current channel, this will notice the user rather than message the channel if they are not voiced or above.",
         }
 
     def load(self):
         self.channels = self.settings.load("channels")
+        if not self.channels:
+            self.channels = {}
 
     def save(self, data=None):
-        self.settings.save_data("channels", {"channels": self.channels})
+        self.settings.save_data("channels", self.channels)
 
     def privmsg(self, data):
         user = data['user']
@@ -49,47 +53,62 @@ class plugin(object):
         message = data['message']
 
         if not self.channels.has_key(channel):
-            self.channels[channel] = "on"
+            self.channels[channel] = {"status": "on", "last": None}
 
-        if self.channels[channel] == "all" or (self.channels[channel] == "on" and (self.irc.is_voice(channel, user) or self.irc.is_op(channel, user))):
-            title, domain = self.pagetitle(message)
+        if not isinstance(self.channels[channel], dict):
+            self.channels[channel] = {"status": self.channels[channel], "last": None}
+
+        if self.channels[channel]["status"] == "all" or (self.channels[channel]["status"] == "on" and (self.irc.is_voice(channel, user) or self.irc.is_op(channel, user))):
+            title, domain = self.pagetitle(message, channel)
             if not title is None:
                 self.irc.sendmsg(channel, "\"%s\" at %s" % (title, domain))
 
     def url_options(self, user, channel, arguments):
         if len(arguments) == 1:
             if not self.channels.has_key(channel):
-                self.channels[channel] = "on"
-            self.irc.sendnotice(user, "URL title fetching for this channel is %s" % (self.channels[channel]))
+                self.channels[channel]["status"] = "on"
+            if not isinstance(self.channels[channel], dict):
+                self.channels[channel] = {"status": self.channels[channel], "last": None}
+            self.irc.sendnotice(user, "URL title fetching for this channel is %s" % (self.channels[channel]["status"]))
         elif len(arguments) == 2:
             arguments[1] = arguments[1].lower()
             if arguments[1] == "on" or arguments[1] == "true":
-                self.channels[channel] = "on"
+                self.channels[channel]["status"] = "on"
             elif arguments[1] == "off" or arguments[1] == "false":
-                self.channels[channel] = "off"
+                self.channels[channel]["status"] = "off"
             elif arguments[1] == "all":
-                self.channels[channel] = "all"
+                self.channels[channel]["status"] = "all"
             else:
                 self.irc.sendnotice(user, "Invalid option given for title matching")
                 return
-            self.irc.sendnotice(user, "URL title fetching for this channel has been set to %s" % (self.channels[channel]))
+            self.irc.sendnotice(user, "URL title fetching for this channel has been set to %s" % (self.channels[channel]["status"]))
 
 
     def fetch_title(self, user, channel, arguments):
+        if not self.channels.has_key(channel):
+            self.channels[channel]["status"] = "on"
+        if not isinstance(self.channels[channel], dict):
+            self.channels[channel] = {"status": self.channels[channel], "last": None}
         if len(arguments) == 2:
-            title, domain = self.pagetitle(arguments[1])
+            title, domain = self.pagetitle(arguments[1], channel)
             if title is None:
                 self.irc.sendnotice(user, "No title or not a URL")
             else:
-                if self.channels[channel] == "all" or (self.channels[channel] == "on" and (self.irc.is_voice(channel, user) or self.irc.is_op(channel, user))):
+                if self.channels[channel]["status"] == "all" or (self.channels[channel]["status"] == "on" and (self.irc.is_voice(channel, user) or self.irc.is_op(channel, user))):
                     self.irc.sendmsg(channel, "\"%s\" at %s" % (title, domain))
                 else:
                     self.irc.sendnotice(user, "\"%s\" at %s" % (title, domain))
-        else:
-            #TODO Show usage
-            pass
+        elif not self.channels[channel]["last"] == None:
+            title, domain = self.pagetitle(self.channels[channel]["last"], channel)
+            if title is None:
+                self.irc.sendnotice(user, "No title or not a URL")
+            else:
+                if self.channels[channel]["status"] == "all" or (self.channels[channel]["status"] == "on" and (self.irc.is_voice(channel, user) or self.irc.is_op(channel, user))):
+                    self.irc.sendmsg(channel, "\"%s\" at %s (%s)" % (title, domain, self.channels[channel]["last"]))
+                else:
+                    self.irc.sendnotice(user, "\"%s\" at %s (%s)" % (title, domain, self.channels[channel]["last"]))
 
-    def pagetitle(self, url):
+    def pagetitle(self, url, channel):
         if not url.split(".")[-1] in self.notParse:
             domain = ""
             try:
@@ -99,6 +118,7 @@ class plugin(object):
                     domain = url.split("https://")[1].split("/")[0]
                 else:
                     return None, None
+                self.channels[channel]["last"] = url
                 br = mechanize.Browser()
                 br.addheaders = [('User-agent',
                               'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9-1.fc9 Firefox/3.0.1')]
@@ -106,6 +126,8 @@ class plugin(object):
                 br.open(url)
                 return br.title(), domain
             except Exception as e:
-                return str(e), domain
+                if not str(e).lower() == "not viewing html":
+                    return str(e), domain
+                return None, None
 
     name = "URL Title Fetcher"
